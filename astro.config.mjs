f0f5a0479d8683e70ad2aaab30f54528c05bf7c9
Ian Dorion <madeindreams@protonmail.com>
2026-04-29 14:17:16 -0400

    resilient app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import resilient from '@yeti/astro-resilient';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  integrations: [
+    resilient({
+      outDir: "dist-light",
+      selector: "main",
+      formats: ["txt", "json"],
+      ipfs: true,
+    }),
+  ],
 });
